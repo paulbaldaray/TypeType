ac36f391dcf5dc85a87e8ac2f67fa9b7bffa27b6
Paul <paulbaldaray@gmail.com>
2019-11-01 22:34:16 -0700

    Fixed bugs

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 window.addEventListener('load', init);
 
-let timeAllowed = 3;
+let timeAllowed = 5;
 let time = timeAllowed;
 let score = 0;
 let isGameOver = false;
+let i1 = null, i2 = null;
 
 usrInput = document.querySelector('.usrInput');
 curr = document.querySelector('.curr');
@@ -11,6 +12,79 @@ scoreShown = document.querySelector('.scoreShown');
 tmShown = document.querySelector('.tmShown');
 msg = document.querySelector('.msg');
 seconds = document.querySelector('.seconds');
+startButton = document.querySelector('.btn-start');
+
+function init() {
+  startButton.addEventListener('click', begin);
+}
+
+function begin() {
+  usrInput.value = "";
+  usrInput.addEventListener('input', play);
+  msg.style.visibility = "hidden";
+  GameOver = false;
+  score = 0;
+  time = timeAllowed;
+  curr.style.visibility = "visible";
+  startButton.style.visibility = "hidden";
+  seconds.innerHTML = timeAllowed;
+  showWord(words);
+  i1 = setInterval(countdown, 1000);
+  if(i2 == null) i2 = setInterval(checkOver, 50);
+}
+
+
+function play() {
+  if(isCorrect()) {
+    isGameOver = false;
+    time = timeAllowed + 1;
+    showWord(words);
+    usrInput.value = '';
+    score++;
+  }
+  scoreShown.innerHTML = score;
+}
+
+function isCorrect() {
+  if(usrInput.value.toLowerCase() === String(typeof(eval(curr.innerHTML))).toLowerCase()) {
+    msg.innerHTML = "Right!"; 
+    msg.style.visibility = "visible";
+    return true;
+  } else {
+    msg.style.visibility = "hidden";
+    return false;
+  }
+}
+
+function showWord(words) {
+  rIdx = Math.floor(Math.random() * words.length);
+  while(curr.innerHTML == words[rIdx]) {
+    rIdx = Math.floor(Math.random() * words.length);
+  }
+  curr.innerHTML = words[rIdx];
+}
+
+function countdown() {
+  if (time > 0) {
+    time--;
+  } else if (time === 0) {
+    isGameOver = true;
+  }
+  tmShown.innerHTML = time;
+}
+
+function checkOver() {
+  if (isGameOver && time === 0) {
+    msg.innerHTML = "Game Over";
+    msg.style.visibility = "visible";
+
+    startButton.innerHTML = "Play Again?";
+    startButton.style.visibility = "visible";
+    curr.style.visibility = "hidden";
+    usrInput.removeEventListener('input', play);
+    clearInterval(i1);
+  }
+}
 
 words = [
   "[]",
@@ -46,56 +120,12 @@ words = [
   "1,'1'",
   "[1,1,1]",
   "[][[]]",
-  "[1]"
+  "[1]",
+  "[1]+[1]",
+  "['a']*['b']",
+  "Number",
+  "'1'+'1'%'1'",
+  "'1'%'1'"
 ];
 
-function init() {
-  seconds.innerHTML = timeAllowed;
-  showWord(words);
-  usrInput.addEventListener('input', start);
-  setInterval(countdown, 1000);
-  setInterval(checkOver, 50);
-}
 
-function start() {
-  if (isCorrect()) {
-    isGameOver = false;
-    time = timeAllowed + 1;
-    showWord(words);
-    usrInput.value = '';
-    score++;
-  }
-  scoreShown.innerHTML = score;
-}
-
-function isCorrect() {
-  if(usrInput.value.toLowerCase() === String(typeof(eval(curr.innerHTML))).toLowerCase()) {
-    msg.innerHTML = "You're Right!"; 
-    msg.style.visibility = "visible";
-    return true;
-  } else {
-    msg.style.visibility = "hidden";
-    return false;
-  }
-}
-
-function showWord(words) {
-  rIdx = Math.floor(Math.random() * words.length);
-  curr.innerHTML = words[rIdx];
-}
-
-function countdown() {
-  if (time > 0) {
-    time--;
-  } else if (time === 0) {
-    isGameOver = true;
-  }
-  tmShown.innerHTML = time;
-}
-
-function checkOver() {
-  if (isGameOver && time === 0) {
-    msg.innerHTML = 'Game Over';
-    msg.style.visibility = "visible";
-  }
-}
